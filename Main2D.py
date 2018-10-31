c3935ca78652b64d1ea06e5374f163c55815b763
amorea12 <antoine81518@gmail.com>
2018-10-31 17:58:39 +0100

    code allégé pour les sauvegardes-chargements

--- Main2D.py
+++ Main2D.py
@@ -56,7 +56,7 @@ L=R2d.RSAA_ph_dist2D([R2d.eucl2D,R2d.vol2D],#geom : géométrie
 1000000,#temps : plus grand temps d'éxécution accepté
 'per')#périodicité
 
-LL=R2d.RSAA_ph_dist2D([R2d.eucl2D,R2d.vol2D],2,[R2d.g_norm,R2d.g_norm],[(3,1),(1.5,5)],[0.15,0.12],[100,100],10000,'per')
+LL=R2d.RSAA_ph_dist2D([R2d.eucl2D,R2d.vol2D],2,[R2d.g_norm,R2d.g_norm],[(3,1),(1.5,5)],[0.15,0.12],[150,150],20000,'per')
 
 LLL=R2d.RSAA_ph_dist2D([R2d.eucl2D,R2d.vol2D],#
 2,#
@@ -69,75 +69,36 @@ LLL=R2d.RSAA_ph_dist2D([R2d.eucl2D,R2d.vol2D],#
 
 #stockage
 
-###exemple
+###sauvegardes : listes de centres-rayons-phases
+##chargement sous un nom commun
 
 ma.dump(LLL, open("L_20", 'wb')) ## Sauvegarde de la liste 
 L_load = ma.load(open("L_20", "rb")) ## Rechargement de la liste
 
 # sauvegarde de la variable maliste sous le nom "maliste" dans le fichier 'L_20_10000'
-with sh.open('L_100_10000') as d_sto:
+with sh.open('L_150_20000') as d_sto:
     d_sto["maliste"] = LL
  
 # chargement de la variable maliste stockée dans le fichier 'L_20_10000'
-with sh.open('L_100_10000') as d_loa:
-    LL_loa = d_loa["maliste"]
-
-###cas d'une grande liste
-
-# sauvegarde de la variable maliste sous le nom "maliste" dans le fichier 'L_1000_1000000'
-with sh.open('L_1000_1000000') as d_sto:
-    d_sto["maliste"] = L
- 
-# chargement de la variable maliste stockée dans le fichier 'L_1000_1000000'
-with sh.open('L_1000_1000000') as d_loa:
+with sh.open('L_150_10000') as d_loa:
     L_loa = d_loa["maliste"]
 
-#test
-
-M=L_loa[0]
-
-N=M[0:10]
-
-#succès
-
-################## Représentation graphique ##################
-
-AA_loa=R2d.Vremp2D(#
-LL_loa,#
-R2d.eucl2D,[100,100],'per')
-
-A=R2d.remp2D(#
-L,#
-R2d.eucl2D,[100,100],'per')
-
-A_loa=R2d.remp2D(#
-LL_loa,#
-R2d.eucl2D,#
-[20,20],#
-'per')
+###--------------- représentation graphique ---------------###
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-########################################"
+#A_remp=R2d.Vremp2D(#
+#L_loa,#
+#R2d.eucl2D,#
+#[1000,1000],#
+#'per')
 
 ### plus rapide ###
 
-#BBB=R2d.Vremp2D(LLL,R2d.eucl2D,[20,20],'per')
 
-A=R2d.Vremp2D(#
-L,#
-R2d.eucl2D,[1000,1000],'per')
+A_remp=R2d.Vremp2D(#
+L_loa,#
+R2d.eucl2D,[150,150],'per')
 
 # Couleurs : bleu pour le fluide, gris pour une première inclusion, jaune pour une deuxième
 ##avec pylab
@@ -146,27 +107,21 @@ bounds=[0,1,2,3]
 norm = mpl.colors.BoundaryNorm(bounds, cmap.N)
 #
 
-pl.imshow(AA_loa,interpolation='none',cmap=cmap,norm=norm)
+#AA_loa=np.reshape(AA_loa,(50,50))#il faudra apporter une correction à R2d.Vremp2D
+
+pl.imshow(A_remp,interpolation='none',cmap=cmap,norm=norm)
 pl.axis('off')
 
 pl.show()
 
 #
+###stockase éventuel
 
-pl.imshow(BBB,interpolation='none',cmap=cmap,norm=norm)
-pl.axis('off')
+with sh.open('VA_150_10000') as d_sto:
+    d_sto["maliste"] = A_remp
 
-pl.show()
-
-
-#
-
-pl.imshow(AA,interpolation='none',cmap=cmap,norm=norm)
-pl.axis('off')
-
-pl.show()
-
-#
+#with sh.open('VA_1000_1000000') as d_loa:
+#    A_loa = d_loa["maliste"]
 
 pl.imshow(A,interpolation='none',cmap=cmap,norm=norm)
 pl.axis('off')
