008120cc414b7654d4973be5681d800e1be5c277
amorea12 <antoine81518@gmail.com>
2018-11-23 17:55:07 +0100

    Etape II : interpolation ok, première ébauche de calcul distribué pour la matrice des snapshots

--- Main3D.py
+++ Main3D.py
@@ -14,9 +14,11 @@ from fenics import *
 from dolfin import *
 from mshr import *
 import matplotlib.pyplot as plt
-#import numpy as np
+import numpy as np
 from math import sqrt
 import sys
+
+import multiprocessing
 
 #from DDD_fun_obj import *
 import DDD_fun_obj as F3d
@@ -71,14 +73,15 @@ V_fixe=VectorFunctionSpace(mesh_fixe, "P", 2, constrained_domain=PeriodicBoundar
 #représentation graphique du maillage
 plot(mesh_fixe)
 plt.show()
+plt.close()
 
 ### Etape 1 : snapshots. Tests. ###
 
-c_x=0.15
-c_y=0.15
-c_z=0.15
+c_x=0.5
+c_y=0.5
+c_z=0.5
 
-r=0.4
+r=0.3
 
 res=10
 
@@ -92,24 +95,99 @@ plt.close()
 
 # Champ khi unique #
 
+res=20
+
 u=F3d.snapshot_sph_per([c_x,c_y,c_z],r,res)
 
 plot(u)
 plt.show()
 plt.close()
 
-## Interpolation du champ ##
-
 ## Erreur de périodicité ##
-
-
-
-
-
-
 
 # Boucles pour une famille de snapshots : variation du rayon ou du centre #
 
+
+## Etape II : extrapolation des clichés, domaine_fixe ##
+
+# Attention aux codes de Hassan : erreurs ... visibles sur les figures du rapport, s'il s'agit bien des snapshots extrapolés
+
+c_x=0.5
+c_y=0.5
+c_z=0.5
+
+res=15
+
+for i in range(1,2):
+ r=i*0.2#05
+ u=F3d.snapshot_sph_per([c_x,c_y,c_z],r,res)
+ ## chargement du snapshot pour l'indice courant
+ # Extrapolation au domaine Omega_fixe : aucune inclusion, khi défini sur [0,1]times[0,1]
+ u.set_allow_extrapolation(True)
+ u_fixe=interpolate(u,V_fixe)##rapide
+ u_fixe = project(u, V_fixe)##lent
+ plot(u_fixe)
+ plt.show()
+ plt.close()
+
+## Etape III : en utilisant la méthode des snapshots, calcul de la POD et des coefficients aléatoires, toujours dans domaine_fixe ##
+
+import Antoine_POD as pod
+pod = reload(pod)
+#from Antoine_POD import *
+
+# Domaine d'interpolation et matrice des snapshots
+
+c_x=0.5
+c_y=0.5
+c_z=0.5
+
+res=15
+
+Nsnap=8
+
+V_fixe=VectorFunctionSpace(mesh_fixe, "P", 2, constrained_domain=PeriodicBoundary())
+nb_noeuds = V_fixe.dim()
+
+Usnap=np.zeros((nb_noeuds,Nsnap))
+
+pas=0.05
+def inter_snap_ray(n):
+ r=n*pas
+ # Cliché sur le domaine avec inclusion
+ u=F3d.snapshot_sph_per([c_x,c_y,c_z],r,res)
+ # Extrapolation du cliché : khi prime
+ u.set_allow_extrapolation(True)
+ u_fixe=interpolate(u,V_fixe)
+ ## Remplissage de la matrice des snapshots
+ #Usnap[:,n-1]=u_fixe.vector().get_local()
+ return([n,u_fixe])
+
+# Remplissage séquentiel de la matrice des snapshots
+for n in range(1,1+Nsnap):
+ u_fixe=inter_snap_ray(n)[1]
+ Usnap[:,n-1]=u_fixe.vector().get_local()
+
+# Remplissage paralèle de la matrice des snapshots
+pool=multiprocessing.Pool(processes=8)
+
+Uf_par=pool.map(inter_snap_ray,(n for n in range(1,1+Nsnap)))
+
+for n in range(1,1+Nsnap):
+ for i in range(0,Nsnap):
+  if Uf_par[i][0]==n:
+   u_fixe=Uf_par[i][1]
+   Usnap[:,n]=u_fixe.vector().get_local()
+
+# matrice de corrélation
+C=pod.mat_corr_temp(V_fixe,Nsnap,Usnap)
+
+# Calcul des coefficients aléatoires et la base POD
+vp_A_phi=pod.mat_a_mat_phi(Nsnap,Usnap,C)
+
+val_propres=vp_A_phi[0]
+Aleat=vp_A_phi[1]
+phi=vp_A_phi[2]
 
 
 
