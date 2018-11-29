efb0940ce106fa34739cdaf14b929b4018a0aaeb
amorea12 <antoine81518@gmail.com>
2018-11-29 19:05:37 +0100

    Tests et affichage des fonctions POD

--- Main2D.py
+++ Main2D.py
@@ -212,7 +212,10 @@ F2d=reload(F2d)
 
 ### Codes éxécutés : cas d'une inclusion circulaire, le rayon du disque central est le paramètre pour la POD ###
 
-## Etape I : réalisation des clichés, avec la méthode des éléments finis. Stockage dans snap2D/ ##
+#######################################################################################################################################
+## Etape I : réalisation des clichés, avec la méthode des éléments finis. Calcul du tenseur d'homogénéisation. Stockage dans snap2D/ ##
+#######################################################################################################################################
+
 # Utilise fenics, éventuellement augmenté par dolfin #
 
 #Caractérisation de la cellule élémentaire, tolérance ??
@@ -253,13 +256,6 @@ plot(mesh_fixe)
 plt.show()
 plt.close()
 
-#stockage dans Mesh2D
-
-mesh_name='mesh_'+'fixe'+'_'+str(res_fixe)
-
-rep_inc='Mesh2D'
-#sys.exit("fin temporaire")
-
 # Famille de cellules élémentaires : 8 clichés, inclusion circulaire, paramétrée par le rayon du cercle
 
 c_x=0.5
@@ -274,67 +270,15 @@ Npas=4
 #l_err_abs=[]
 #l_err_rel=[]
 
-for i in range(1,6):#[0.111,0.211,0.316,0.423]:#,0.49]:#range(1,2):#9):#attention le rayon d'un cercle doit être non nul
+for i in range(1,8):#[0.111,0.211,0.316,0.423]:#,0.49]:#range(1,2):#9):#attention le rayon d'un cercle doit être non nul
  r=i*0.05
- c_x=0.2#i*0.1
- c_y=0.2#i*0.1
+ c_x=0.5#i*0.1
+ c_y=0.5#i*0.1
  u=F2d.snapshot_circ_per([c_x,c_y],r,res)
  # Représentation graphique
- #plot(u)
- #plt.show()
- #plt.close()
- # erreurs de périodicité, point par point
- pas=1/Npas
- #l_x_0=array([u((0,k*pas)) for k in range(0,Npas)])
- #l_x_1=array([u((1,k*pas)) for k in range(0,Npas)])
- #list_per_x=[sum((u((1,pas*k))-u((0,pas*k)))**2) for k in range(0,Npas)]
- #l_x_diff=l_x_1-l_x_0
- #El2_per_x=sqrt(sum(list_per_x)/Npas)/1.0#den[0]
- #list_0_x=[sum(u((0,pas*k))**2) for k in range(0,Npas)]
- #El2_rel_per_x=sqrt(sum(list_per_x)/Npas)/sqrt(sum(list_0_x)/Npas)
- #print('Différence en x :',l_x_diff)
- #print('Différence et valeur quadratiques en x :',list_per_x,list_0_x)
- #print('Différence en moyenne quadratique pour x :',El2_per_x)
- #print('Différence relative en moyenne quadratique pour x :',El2_rel_per_x)
- #l_y_0=array([u((0.1*k,0)) for k in range(0,10)])
- #l_y_1=array([u((0.1*k,1)) for k in range(0,10)])
- #l_y_diff=l_y_1-l_y_0
- #print('Différence en y :',l_y_diff)
- ##
- ### Extrapolation au domaine entier : [0,1]^2 ###
- #u_fixe=u
- ## erreurs d'extrapolation, absolue et relative
- u.set_allow_extrapolation(True)
- u_fixe=interpolate(u,V_fixe)
- #plot(u_fixe)
- #plt.show()
- #plt.close()
- # Périodicité
- print('khiprime au bord vertical :')
- for k in range(0,1+Npas):
-  print(u_fixe((0,pas*k)),u_fixe((1,pas*k)))
- print('khiprime au bord horizontal :')
- for k in range(0,1+Npas):
-  print(u_fixe((pas*k,0)),u_fixe((pas*k,1)))
- #print('u',err_per_01(u,'l2',100,'rel'))
- #print('ufixe',err_per_01(u_fixe,'l2',100,'rel'))
- # Erreur d'interpolation
- u_mat=np.zeros((1+Npas,1+Npas))
- u_diff=np.zeros((1+Npas,1+Npas))
- #for k in range(0,1+Npas):
- # for l in range(0,1+Npas):
- #  u_diff[k,l]=sum([x**2 for x in u((pas*k,pas*l))])-sum([x**2 for x in u_fixe((pas*k,pas*l))])
- #  u_mat[k,l]=sum([x**2 for x in u((pas*k,pas*l))])
- #u_arr=np.reshape(u_mat,(1+Npas)*(1+Npas))
- #u_diff_ligne=np.reshape(u_diff,(1+Npas)*(1+Npas))
- #u_rel_ligne=u_diff_ligne/u_arr
- #diff_unif=max(u_diff_ligne)
- #err_u_rel=max(u_rel_ligne)#diff_unif/u_min_unif
- #l_err_abs.append(('rayon',r,'abs',diff_unif))
- #l_err_rel.append(('rayon',r,'erreur relative maximum',err_u_rel))
- #print('erreur interpolation',diff_unif)
- #print('erreur relative interpolation',err_u_rel)
- #print('centre de la cellule',u((0.5,0.5)),u_fixe((0.5,0.5)))
+ plot(u)
+ plt.show()
+ plt.close()
  ##
  # Tenseur d'homogéisation
  ## Intégrale de khi sur le domaine fluide
@@ -352,33 +296,9 @@ for i in range(1,6):#[0.111,0.211,0.316,0.423]:#,0.49]:#range(1,2):#9):#attentio
  # Stockage
  ## ...
 
-## Erreurs d'interpolation : c_x, c_y = 0.5,0.5
-#print('abs',l_err_abs)
-for i in range(0,8):
- print(l_err_rel[i])
-## 0.05 à 0.4, suite arithmétique :
-#('rayon', 0.05, 'erreur relative maximum', 0.0011250471700128834)
-#('rayon', 0.1, 'erreur relative maximum', 0.0024562097480711302)
-#('rayon', 0.15000000000000002, 'erreur relative maximum', 0.0079380966355682512)
-#('rayon', 0.2, 'erreur relative maximum', 0.031271739691666682)
-#('rayon', 0.25, 'erreur relative maximum', 0.021105098011912495)
-#('rayon', 0.30000000000000004, 'erreur relative maximum', 0.032643098011902655)
-#('rayon', 0.35000000000000003, 'erreur relative maximum', 0.059268219555300893)
-#('rayon', 0.4, 'erreur relative maximum', 0.16544547860510037)
-## -- End pasted text --
-
-## Erreurs d'interpolation : c_x, c_y = 0.2,0.2 ; suite arithmétique de raison etpremier terme 0.05
-#print('abs',l_err_abs)
-for i in range(0,5):
- print(l_err_rel[i])
-#('rayon', 0.05, 'erreur relative maximum', 0.30378848343770709)
-#('rayon', 0.1, 'erreur relative maximum', 0.49584821840470178)
-#('rayon', 0.15000000000000002, 'erreur relative maximum', 0.57479441504707507)
-#('rayon', 0.2, 'erreur relative maximum', 1.4487937386039291e-09)
-#('rayon', 0.25, 'erreur relative maximum', 3.0524331809039409e-08)
-## -- End pasted text --
-
-## Etape II : extrapolation des clichés, domaine_fixe ##
+#####################################################################################################################################
+######################################### Etape II : extrapolation des clichés, domaine_fixe ########################################
+#####################################################################################################################################
 
 # Attention aux codes de Hassan : erreurs ... visibles sur les figures du rapport, s'il s'agit bien des snapshots extrapolés
 
@@ -404,7 +324,9 @@ for i in range(1,2):
  plt.show()
  plt.close()
 
+####################################################################################################################################
 ## Etape III : en utilisant la méthode des snapshots, calcul de la POD et des coefficients aléatoires, toujours dans domaine_fixe ##
+####################################################################################################################################
 
 #mesh_fixe = Mesh("Solutions_homog_interp_circulaire/mesh_circulaire.xml.gz")
 
@@ -469,12 +391,51 @@ C=pod.mat_corr_temp(V_fixe,Nsnap,Usnap)
 
 # Calcul des coefficients aléatoires et la base POD
 
-vp_A_phi=pod.mat_a_mat_phi(Nsnap,Usnap,C)
+vp_A_phi=pod.mat_a_mat_phi(Nsnap,Usnap,C,V_fixe,'n2')
+vp_A_phi=pod.mat_a_mat_phi(Nsnap,Usnap,C,V_fixe,'L2')
+#vp_A_phi=pod.mat_a_mat_phi(Nsnap,Usnap,C,'')
 
 val_propres=vp_A_phi[0]
 Aleat=vp_A_phi[1]
-phi=vp_A_phi[2]
+## Attention les objets rangés dans tableau suivant sont des vecteurs
+Phi_prime_v=vp_A_phi[2]
 
+## Tests : orthogonalité ou orthonrmalité de Phi_prime
+ui=Function(V_fixe)
+uj=Function(V_fixe)
+
+for i in range(Nsnap-1):
+ ui.vector().set_local(Phi_prime_v[:,i])
+ for j in range(i+1,Nsnap):
+  uj.vector().set_local(Phi_prime_v[:,j])
+  scal=assemble(dot(ui,uj)*dx)
+  print(scal)
+
+for i in range(Nsnap):
+ ui.vector().set_local(Phi_prime_v[:,i])
+ scal=assemble(dot(ui,ui)*dx)
+ norme_L2=sqrt(scal)
+ ###
+ norme_q=0
+ l=len(Phi_prime_v[:,i])
+ for k in range(l):
+  norme_q+=Phi_prime_v[k,i]**2
+ norme_2=sqrt(norme_q)
+ #print('norme 2 :',norme_q)
+ print('norme L2 :',norme_L2)
+ #print('quotient n2/L2 :',scal/norme_q)
+
+# Représentation graphique des vecteurs de POD :
+
+## Type de données : on veut calculer les fonctions phi_prime_i 
+## Représentation graphique des phi_prime_i :
+
+phi=Function(V_fixe)
+for i in range(Nsnap):
+ phi.vector().set_local(Phi_prime_v[:,i])
+ plot(phi)
+ plt.show()
+ plt.close()
 
 
 ## Etape IV : Prédictions ##
