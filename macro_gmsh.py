1a308c06767227ac1c9d5d3a95f428f190ccc7c0
Antoine Moreau <antoine81518@gmail.com>
2019-02-14 00:43:21 +0100

    correction de la fusion : elif test

--- macro_gmsh.py
+++ macro_gmsh.py
@@ -35,7 +35,7 @@ config='sph_un'
 
 Nsnap=1
 
-config='cer_un_som'#cer_un
+config='cer_un'#'cer_un_som'#
 
 res=1# valeur par défaut, jamais utilisée
 #res=20
@@ -121,27 +121,7 @@ elif test:
   if res_name and dimension==3:
    mesh_name=mesh_name+"sur"+str(res)
   ## res=10 : pas de suffixe dans le cas sphérique, voir avec res_name
-  ###
- if dimension==2:
-  mesh_name="maillage_fixe2D_am"
-  ## Génération d'un fichier .geo ? On commence avec un fichier unique et on modifie geo_p dans le code avant de sauvegarder sous le nom courant.
-  print(mesh_name)
-  ## Visualisation du fichier .geo
-  print("gmsh "+mesh_name+".geo")
-  os.system("gmsh "+mesh_name+".geo")
-  ## Conversion en .msh
-  os.system("gmsh -"+str(dimension)+" "+mesh_name+".geo")
-  ## Affichage du maillage obtenu
-  if fig_todo=='aff':
-   os.system("gmsh "+mesh_name+".msh")
-  ## Conversion en .xml avec dolfin pour FEniCS
-  os.system("dolfin-convert "+mesh_name+".msh "+mesh_name+".xml")
-elif test:
- for r in [0.22,0.33,0.44]:
   mesh_name=mesh_prefix+"_"+str(int(round(100*r,2)))
-  if res==20:
-   mesh_name=mesh_name+"_res"+str(res)
-  ## res=100 : pas de suffixe
   ## Génération d'un fichier .geo ? On commence avec un fichier unique et on modifie geo_p dans le code avant de sauvegarder sous le nom courant.
   print(mesh_name)
   ## Visualisation du fichier .geo
