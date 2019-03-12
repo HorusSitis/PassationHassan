24bebea7b0fe2c5ed4acd7481b0d944f74e66a46
Antoine Moreau <antoine81518@gmail.com>
2019-03-12 22:05:18 +0100

    script pouvant être appelé plusieurs fois successives

--- DDD_EIV.py
+++ DDD_EIV.py
@@ -1,8 +1,7 @@
-#################################################################################################
-## Etape IV : Prédictions. Choisir les paramètres du problème à résoudre par le modèle réduit. ##
-#################################################################################################
 
-### ------------ Reproduire éventuellement pour des étapes ultérieures. Laisser seulement dans DDD_fun_obj ? ------------ ###
+# -*- coding: Latin-1 -*-
+#################################################################################################
+## Etape IV : Predictions. Choisir les parametres du probleme a resoudre par le modele reduit.#
 
 tol=1e-10
 
@@ -44,7 +43,8 @@ class PeriodicBoundary(SubDomain):
 
 mesh_dir="maillages_per/3D/"
 
-## inclusions simples ou rayons liés
+## inclusions simples ou rayons lies
+
 if dom_fixe=="am":
  mesh_f_name=mesh_dir+"cube_periodique_triangle"+"_"+dom_fixe+"_sur"+str(res_gmsh)+"_fixe.xml"
 ## inclusions multiples, unique rayon variable
@@ -53,7 +53,7 @@ elif dom_fixe=="solid":
  if config=='2sph':
   mesh_f_name=mesh_fixe_prefix+"fixe"+str(int(round(100*r_v_0,2)))+"sur"+str(res_gmsh)+".xml"
  elif config=='cylsph':
-  ## rayon du cylindre aux arètes ou de la sphère centrale fixés à 0.15 ##
+  ## rayon du cylindre aux aretes ou de la sphere centrale fixes a 0.15 ##
   if geo_p=='ray_sph':
    mesh_f_name=mesh_fixe_prefix+str(int(round(100*r_c_0,2)))+"fixe"+"sur"+str(res_gmsh)+".xml"
   elif geo_p=='ray_cyl':
@@ -62,7 +62,7 @@ elif dom_fixe=="solid":
    elif fixe_comp=='sph_un':
     mesh_f_name=mesh_dir+"cubesphere_periodique_triangle_"+str(int(round(100*r_s_0,2)))+"sur"+str(res_gmsh)+".xml"
 elif dom_fixe=="ray_min":
- fixe_comp=True#utilisation du domaine fixe avec annulation du rayon du cylindre dans le fichier général
+ fixe_comp=True#utilisation du domaine fixe avec annulation du rayon du cylindre dans le fichier general
  if config=='cylsph':
   if geo_p=='ray_sph':
    mesh_f_name=mesh_dir+"cube"+config+"_periodique_triangle_"+str(int(round(100*r_c_0,2)))+str(int(round(100*r_min,2)))+"sur"+str(res_gmsh)+".xml"
@@ -79,12 +79,10 @@ V_fixe=VectorFunctionSpace(mesh_fixe,'P',2,constrained_domain=PeriodicBoundary()
 
 import time
 
-### ------------ Etapes reproduites : dépendances directes de Main3D ------------ ###
+### ------------ Etapes reproduites : dependances directes de Main3D ------------ ###
 
-#r_nouv=0.22
 nb_modes=N_mor
 
-#mesh_dir="maillages_per/3D/"
 if config=='sph_un':
  mesh_n_name=mesh_dir+"cubesphere_periodique_triangle_"+str(int(round(100*r_nouv,2)))+"sur"+str(res_gmsh)
 elif config=='cyl_un':
@@ -96,15 +94,10 @@ elif config=='cylsph':
   mesh_n_name=mesh_dir+"cube"+config+"_periodique_triangle_"+str(int(round(100*r_c_0,2)))+str(int(round(100*r_nouv,2)))+"sur"+str(res_gmsh)
  elif geo_p=='ray_cyl':
   mesh_n_name=mesh_dir+"cube"+config+"_periodique_triangle_"+str(int(round(100*r_nouv,2)))+str(int(round(100*r_s_0,2)))+"sur"+str(res_gmsh)
- #elif geo_p=='ray_linked':
-
-#print("Maillage fixe : ",mesh_f_name)
-#print(mesh_n_name)
 
 mesh_nouv=Mesh(mesh_n_name+".xml")
 
 V_nouv=VectorFunctionSpace(mesh_nouv, "P", 2, constrained_domain=PeriodicBoundary())
-
 
 
 # --------------------- SE1 : projection de la base POD sur le nouveau domaine --------------------- #
@@ -117,7 +110,7 @@ start=time.time()
 
 nb_noeuds_fixe=V_fixe.dim()
 
-## Chargement de la base POD complète
+## Chargement de la base POD complete
 
 if ind_res:
  phi_name='Phi'+dom_fixe+'_dim'+str(Nsnap)+'_'+config+'_'+geo_p+'_'+"res"+str(res)+'_'+ordo+'_'+computer
@@ -126,16 +119,15 @@ elif ind_fixe:
 else:
  phi_name='Phi'+'_dim'+str(Nsnap)+'_'+config+'_'+geo_p+'_'+ordo+'_'+computer
 
-#print(phi_name)
 
 with sh.open(repertoire_parent+phi_name) as phi_loa:
  Phi_prime_v = phi_loa["maliste"]
 
-## Création de la base POD tronquée, sous forme vectorielle
+## Creation de la base POD tronquee, sous forme vectorielle
 
 Phi_mor=Phi_prime_v[:,range(0,nb_modes)]
 
-## Extrpolation des fonctions de la base POD pour former le modèle réduit défini sur V_nouv
+## Extrpolation des fonctions de la base POD pour former le modele reduit defini sur V_nouv
 
 nb_noeuds_nouv=V_nouv.dim()
 Phi_nouv_v=np.zeros((nb_noeuds_nouv,nb_modes))
@@ -149,45 +141,41 @@ for n in range(0,nb_modes):
  # extrapolation du snapshot au domaine fixe
  phi_fixe.set_allow_extrapolation(True)
  phi_n_nouv=interpolate(phi_fixe,V_nouv)
- # on range le vecteur de POD interpolée dans la matrice Phi_nouv_v
+ # on range le vecteur de POD interpolee dans la matrice Phi_nouv_v
  Phi_nouv_v[:,n]=phi_n_nouv.vector().get_local()
- #sys.exit('interpolation')
 
-## Stockage de la matrice du modèle réduit
+## Stockage de la matrice du modele reduit
 
-## On enregistre et imprime le temps d'éxécution de SE1
+## On enregistre et imprime le temps d'execution de SE1
 
 end=time.time()
 
 t_phi_nouv=end-start
 
 print('se1 faite ',t_phi_nouv,' secondes')
-#sys.exit()#-------------------------------------
-# --------------------- SE2 : résolution du modèle réduit --------------------- #
 
-## On réinitialise le temps de calcul ##
+# --------------------- SE2 : resolution du modele reduit --------------------- #
+
+## On reinitialise le temps de calcul ##
 
 start=time.time()
 
-## On écrit les deux tenseurs qui comportent les coefficients de l'équation du modèle réduit : ceux-ci dépendent des vecteurs de la base POD projetée
-
-#from PO23D import *
-
+## On ecrit les deux tenseurs qui comportent les coefficients de l'equation du modele reduit : ceux-ci dependent des vecteurs de la base POD projetee
 
 if config=='sph_un' or config=='cyl_un':
  Coeff=calc_Ab_3D(V_nouv,mesh_nouv,Phi_nouv_v,r_nouv,cen_snap_ray,nb_modes,config)
 else:
  Coeff=calc_Ab_compl_3D(mesh_n_name,Phi_nouv_v,nb_modes)
-#sys.exit("solveur ROM éxécuté pour des inclusions multiples")
+#sys.exit("solveur ROM execute pour des inclusions multiples")
 A=Coeff[0]
 b=Coeff[1]
 
 print('A :',A,'b :',b)
-## On résoud le modèle réduit
+## On resoud le modele reduit
 
 a_nouv=np.linalg.solve(A.T,-b)
 
-## On enregistre et imprime le temps d'éxécution de SE2
+## On enregistre et imprime le temps d'execution de SE2
 
 end=time.time()
 
@@ -196,7 +184,7 @@ t_rom_linear=end-start
 print('se2 faite ',t_rom_linear,' secondes')
 # --------------------- SE3 : calcul du nouveau champ de vecteurs, affichage --------------------- #
 
-## On réinitialise le temps de calcul ##
+## On reinitialise le temps de calcul ##
 
 start=time.time()
 
@@ -218,7 +206,7 @@ plt.close()
 r=r_nouv
 rho=r_nouv
 
-# Affichage des valeurs et erreurs de la solution périodique, quelle que soit la configuration
+# Affichage des valeurs et erreurs de la solution periodique, quelle que soit la configuration
 
 if config=='sph_un' or config=='cyl_un':
  #err_per_ind_01(chi_n,cen,r,npas_err)
@@ -233,15 +221,15 @@ elif config=='cylsph':
  #elif geo_p=='ray_linked':
 
 
-# Tenseur de diffusion homogénéisé
-## Intégrale de chi sur le domaine fluide
+# Tenseur de diffusion homogeneise
+## Integrale de chi sur le domaine fluide
 T_chi=np.zeros((3,3))
 for k in range(0,3):
  for l in range(0,3):
   T_chi[k,l]=assemble(grad(chi_nouv)[k,l]*dx)
-  #print("Intégrale ",assemble(grad(chi_nouv)[k,l]*dx))
-## Intégrale de l'identité sur le domaine fluide
-### Calcul de la porosité
+  #print("Integrale ",assemble(grad(chi_nouv)[k,l]*dx))
+## Integrale de l'identite sur le domaine fluide
+### Calcul de la porosite
 if config=='sph_un':
  por=1-4/3*pi*r_nouv**3
 elif config=='cyl_un':
@@ -257,28 +245,28 @@ elif config=='cylsph':
   r_c=r_nouv
  #elif geo_p=='ray_linked':
  por=1-4/3*pi*r_s**3-pi*r_c**2
-### Intégration du terme constant du coefficient d diffusion, sur le domaine fluide
+### Integration du terme constant du coefficient d diffusion, sur le domaine fluide
 D=por*np.eye(3)
 ## Calcul et affichage du tenseur Dhom
 Dhom_kMOR=D_k*(D+T_chi.T)
 #print(('Tenseur Dhom_k',Dhom_k))
 print('Coefficient Dhom_k11 '+conf_mess+', '+geo_mess+' valeur '+str(rho)+' MOR :',Dhom_kMOR[0,0])
 
-## On enregistre et imprime le temps d'éxécution de SE3
+## On enregistre et imprime le temps d'execution de SE3
 
 end=time.time()
 
 t_rom_Dhom=end-start
 
 print('se3 faite ',t_rom_Dhom,' secondes')
-#sys.exit()#-------------------------------------
-# --------------------- SE4 : comparaison avec la méthode des éléments finis --------------------- #
 
-## On réinitialise le temps de calcul ##
+# --------------------- SE4 : comparaison avec la methode des elements finis --------------------- #
+
+## On reinitialise le temps de calcul ##
 
 start=time.time()
 
-## On réinitialise le champ chi_nouv pour la méthode des éléments finis
+## On reinitialise le champ chi_nouv pour la methode des elements finis
 
 #res=20
 if config=='sph_un':
@@ -306,7 +294,7 @@ elif fig_todo=='save':
  plt.savefig("Figures3D/sol_rom"+str(int(round(100*r_nouv,2)))+"_sur"+str(Nsnap)+config+'_'+geo_p+"res"+str(res)+".png")
 plt.close()
 #sys.exit()
-# Affichage des valeurs et erreurs de la solution périodique, quelle que soit la configuration
+# Affichage des valeurs et erreurs de la solution periodique, quelle que soit la configuration
 if config=='sph_un' or config=='cyl_un':
  #err_per_ind_01(chi_n,cen,r,npas_err)
  err_per_gr(cen_snap_ray,r_nouv,chi_nouv,npas_err,fig_todo)
@@ -319,15 +307,15 @@ elif config=='cylsph':
   err_per_gr_compl(config,r_nouv,chi_nouv,npas_err,fig_todo)
  #elif geo_p=='ray_linked':
 
-# Tenseur de diffusion homogénéisé
-## Intégrale de chi sur le domaine fluide
+# Tenseur de diffusion homogeneise
+## Integrale de chi sur le domaine fluide
 T_chi=np.zeros((3,3))
 for k in range(0,3):
  for l in range(0,3):
   T_chi[k,l]=assemble(grad(chi_nouv)[k,l]*dx)
-## Intégrale de l'identité sur le domaine fluide : voir ce qui précède avec lea porosité
+## Integrale de l'identite sur le domaine fluide : voir ce qui precede avec la porosite
 print('Noeuds :',V_nouv.dim())
-print('Porosité :',por)
+print('Porosite :',por)
 #print('tenseur : ',T_chi)
 ## Calcul et affichage du tenseur Dhom
 Dhom_kMEF=D_k*(D+T_chi.T)
@@ -339,7 +327,7 @@ print('Coefficient Dhom_k11 '+conf_mess+', '+geo_mess+' valeur '+str(rho)+ ' MEF
 err_rel=100*(Dhom_kMOR[0,0]-Dhom_kMEF[0,0])/Dhom_kMEF[0,0]
 print('Erreur relative MEF-MOR :', err_rel , ' pourcent')
 
-## On enregistre et imprime le temps d'éxécution de SE4
+## On enregistre et imprime le temps d'execution de SE4
 
 end=time.time()
 
@@ -348,35 +336,35 @@ t_fem=end-start
 print('se4 faite ',t_fem,' secondes')
 
 ##############################################################################
-########################## Evaluation de la méthode ##########################
+########################## Evaluation de la methode ##########################
 ##############################################################################
 
 if Report :
  print('##############################################################################')
- print('########################## Evaluation de la méthode ##########################')
+ print('########################## Evaluation de la methode ##########################')
  print('##############################################################################')
- print('Résultats '+conf_mess+', '+geo_mess+' valeur '+str(r_nouv)+' :')
+ print('Resultats '+conf_mess+', '+geo_mess+' valeur '+str(r_nouv)+' :')
  print('##############################################################################')
  #
- ## Porosité ##
- print('Porosité :',por)
- ## Dhom, erreur, et temps d'éxécution ##
+ ## Porosite ##
+ print('Porosite :',por)
+ ## Dhom, erreur, et temps d'execution ##
  print('Coefficient Dhom_k11 '+' MOR :',Dhom_kMOR[0,0])
  print('Coefficient Dhom_k11 '+' MEF :',Dhom_kMEF[0,0])
  print('Maillage :',t_meshing,'secondes')
  print('Erreur relative MEF-MOR :',err_rel,'pourcent')
- ## Temps de calcul à évaluer ##
+ ## Temps de calcul a evaluer ##
  print('T_phi_nouv',t_phi_nouv,'secondes')
  print('T_ROM',t_rom_linear+t_rom_Dhom,'secondes')
  print('T_FEM',t_fem,'secondes')
  ## Nombre de noeuds ##
  print('Noeuds :',V_nouv.dim())
- ## Rapports de temps de calcul, sans unité ##
+ ## Rapports de temps de calcul, sans unite ##
  R_rom=(t_phi_nouv+t_rom_linear+t_rom_Dhom+t_meshing)/(t_fem+t_meshing)
  R_interpolation=t_phi_nouv/(t_fem+t_meshing)
- print('Gain de temps de la méthode :',R_rom)#,'sans unité')
- print('Contribution de l interpolation :',R_interpolation)#,'sans unité')
- print('Différence :',R_rom-R_interpolation)#,'sans unité')
+ print('Gain de temps de la methode :',R_rom)#,'sans unite')
+ print('Contribution de l interpolation :',R_interpolation)#,'sans unite')
+ print('Difference :',R_rom-R_interpolation)#,'sans unite')
  #
  print('##############################################################################')
  print('##############################################################################')
