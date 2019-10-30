89e860c5e32634537daacfd399c850e921f40c4e
amorea12 <antoine81518@gmail.com>
2019-10-30 20:29:14 +0100

    options dont resolution des images parapetriques

--- DDD_E0.py
+++ DDD_E0.py
@@ -56,9 +56,6 @@ elif cmap_conv == 'cement':
     fluid_color = 'cyan'
     colors_ph = ['grey','orange']
 
-# import time
-
-
 # caracteristiques de la cellule
 
 xinf = -1.
@@ -71,28 +68,29 @@ xsup = xinf + size
 ysup = yinf + size
 zsup = zinf + size
 
-
 # nombre maximal d'iterations
 
-tps_remp = 50
-
-# repertoire pour les listes d'inclustions
-
-rep_inc = 'IncAlea3D'
-
-## Fabrication de la liste des inclusions etiquetees	numfic,time = py.loadtxt("%s/rayon_ecriture.txt" %(repertoire_init), unpack=True) ##
-
-## Pour generer les inclusions dans une cellule elementaire
+tps_remp = 100
 
 # lois normales pour la taille des inclusions ; parametres #
 
 lois_rayons = [rsa.g_norm, rsa.g_norm]
-par_rayons = [(0.1,0.07), (0.25,0.02)]
+par_rayons = [(0.08, 0.07), (0.18, 0.02)]
 
 # fractions volumiques pour les differentes phases ; marges
 
-frac_vol = [0.15, 0.12]
+frac_vol = [0.15, 0.16]
 delta_inc = 0.05
+
+# Affichage de la cellule elementaire
+
+nb_lcells = 3
+
+u_res = 80
+v_res = 50
+
+### ------------ Pour generer les inclusions dans une cellule elementaire ------------ ###
+# Fabrication de la liste des inclusions etiquetees
 
 list_vol_cell_inc = rsa.RSAA_ph_eucl_cell(delta_inc, lois_rayons, par_rayons, frac_vol, [xinf, yinf, zinf], size, tps_remp, 'per')
 
@@ -100,19 +98,11 @@ list_vol_cell_inc = rsa.RSAA_ph_eucl_cell(delta_inc, lois_rayons, par_rayons, fr
 
 list_cell_inc = list_vol_cell_inc[0]
 
-# Affichage de la cellule elementaire'grey','orange'
-
-nb_lcells = 3
-
-gr_res = 100
-
-
-
-
+### ------------ Affichage d'une microstructure periodique avec la cellule generee aleatoirement ------------ ###
 
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
-# taille du graphique'grey','orange'
+# taille du graphique
 ax.set_xlim((xinf, xinf + nb_lcells*size))
 ax.set_ylim((yinf, yinf + nb_lcells*size))
 ax.set_zlim((zinf, zinf + nb_lcells*size))
@@ -123,9 +113,8 @@ ax.set_zlim((zinf, zinf + nb_lcells*size))
 ax.set_axis_bgcolor(fluid_color)
 
 # pour un parametrage des surfaces
-u = np.linspace(0, 2 * np.pi, gr_res)
-v = np.linspace(0, np.pi, gr_res)
-
+u = np.linspace(0, 2 * np.pi, u_res)
+v = np.linspace(0, np.pi, v_res)
 
 
 # boucle sur les cellules
@@ -150,10 +139,6 @@ for a in range(0,nb_lcells):
                 ax.plot_surface(x, y, z, linewidth=0, antialiased=False, color=colors_ph[phase_index])
 
 
-
-
-
-
 # grille
 ax.xaxis.set_major_locator(plt.MultipleLocator(1.0))
 ax.yaxis.set_major_locator(plt.MultipleLocator(1.0))
@@ -166,40 +151,6 @@ ax.set_yticklabels([])
 ax.set_zticklabels([])
 
 # instruction pour la sortie
-# if fig_todo=='aff':
 plt.title('Inclusions pour '+str(nb_lcells**3)+' periodes, '+str(len(lois_rayons))+' phases solides, '+str(tps_remp)+' iterations RSAA')
 plt.show()
 plt.close()
-
-
-
-
-# Affichage d'une microstructure periodique avec la cellule generee aleatoirement
-
-
-
-
-
-
-
-
-
-
-# # inclusions, placees periodiquement
-# for i in range(0,nb_lcells):
-# for j in range(0,nb_lcells):
-# for k in range(0,nb_lcells):
-# x = 0.5+i+r_sph*np.outer(np.cos(u), np.sin(v))## outer : produit terme à terme
-# y = 0.5+j+r_sph*np.outer(np.sin(u), np.sin(v))
-# z = 0.5+k+r_sph*np.outer(np.ones(np.size(u)), np.cos(v))
-# ax.plot_surface(x, y, z, linewidth=0, antialiased=False, color=cem_color)
-# # grille
-# ax.xaxis.set_major_locator(plt.MultipleLocator(1.0))
-# ax.yaxis.set_major_locator(plt.MultipleLocator(1.0))
-# ax.zaxis.set_major_locator(plt.MultipleLocator(1.0))
-# ax.grid(which='major', axis='x', linewidth=0.85, linestyle='-', color='0.45')
-# ax.grid(which='major', axis='y', linewidth=0.85, linestyle='-', color='0.45')
-# ax.grid(which='major', axis='z', linewidth=0.85, linestyle='-', color='0.45')
-# ax.set_xticklabels([])
-# ax.set_yticklabels([])
-# ax.set_zticklabels([])
